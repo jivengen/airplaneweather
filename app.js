f715b7c4ecc44732358e1d0663fcf36346dc8a21
coolbule64 <jivengen1@buffs.wtamu.edu>
2017-11-30 02:23:52 +0000

    formatted weather

--- app.js
+++ app.js
@@ -20,12 +20,13 @@ var app = express();
 //app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'app_client')));
 
 //route to API calls
 
